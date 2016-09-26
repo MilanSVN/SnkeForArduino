831b77a790b3ec7d26b220c79e3ed7e2127b8168
Milan Bojic <mbojic@domain.local>
2016-09-26 15:08:23 +0200

    Added driver files

---
+++ snake/driver.h
@@ -0,0 +1,11 @@
+#ifndef _DRIVER_H
+#define _DRIVER_H
+#include <stdint.h>
+#include "config.h"
+
+
+void showScreean(uint8_t matrix[PLAYGROUND_Y][PLAYGROUND_X]);
+void setDirection(uint8_t* direction);
+
+
+#endif
--- snake/snake.ino
+++ snake/snake.ino
@@ -4,7 +4,7 @@ extern "C"{
 #include "config.h"
 }
 
-
+static void debugShowDisply(uint8_t arg[PLAYGROUND_Y][PLAYGROUND_X]);
 void TaskGame( void *pvParameters );
 void TaskDisplay( void *pvParameters );
 void TaskButton( void *pvParameters );
@@ -15,6 +15,8 @@ uint8_t gameOver;
 
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("setup begin");
   xTaskCreate(
     TaskGame
     ,  "Game"  // A name just for humans
@@ -56,24 +58,28 @@ void TaskGame(void *pvParameters)  // This is a task.
 
   for (;;) // A Task shall never return or exit.
   {
+    /*
     initGame(&direction,&gameOver);
     while(gameOver == 0)
     {
       runGame();
       vTaskDelay(100 / portTICK_PERIOD_MS);
     }
+    */
   }
 }
 
 void TaskDisplay(void *pvParameters)  // This is a task.
 {
-  Serial.println("TaskGame started");
+  Serial.println("TaskDisplay started");
  
 
   for (;;) // A Task shall never return or exit.
   {
-     
+    /*
+    //debugShowDisply(matrix); 
     vTaskDelay(100 / portTICK_PERIOD_MS);
+    */
   }
 }
 
@@ -84,7 +90,35 @@ void TaskButton(void *pvParameters)  // This is a task.
 
   for (;;) // A Task shall never return or exit.
   {
-
+    /*
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
+    */
   }
 }
 
+/*
+static void debugShowDisply(uint8_t arg[PLAYGROUND_Y][PLAYGROUND_X])
+{
+  uint8_t i = 0;
+  uint8_t j = 0;
+  Serial.write(12);//clearScreen
+  for(i = 0; i < PLAYGROUND_Y; i++)
+  {
+    for(j = 0; j < PLAYGROUND_X; j++)
+      {
+        switch (arg[i][j]) 
+        {
+          case 0:
+            Serial.print("0");
+            break;
+          case 1:
+            Serial.print("8");
+            break;
+          default: 
+            Serial.print("x");
+          break;
+        }
+      }
+      Serial.println();
+  }  
+}*/
